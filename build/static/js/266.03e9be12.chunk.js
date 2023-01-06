1c5be5ac6572263246ff9197a4733b4ff14d7623
Oleg Bukovskiy <olbukovskiy@gmail.com>
2023-01-06 11:41:06 +0200

    changes

---
+++ build/static/js/266.03e9be12.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,n,t){t.r(n),t.d(n,{Home:function(){return c}});var r=t(3329),c=function(){return(0,r.jsxs)("div",{children:["Home page",(0,r.jsx)("div",{children:"Here will be some content"}),(0,r.jsx)("div",{children:"More content"})]})}}}]);
+//# sourceMappingURL=266.03e9be12.chunk.js.map
